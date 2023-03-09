78eb7f03cd4cd515233c464603c88f4c06eb0919
Amine Doumou <doumou.amine@gmail.com>
2023-03-09 21:44:51 +0100

    second  commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,16 +2,16 @@
     @section('main')
         <div class="flex flex-col space-y-4 item-center justify-center w-2/5 h-96 mx-auto font-Roboto_Mono my-4">
             <p>
-                Hi. I’m one of the luckiest people you’re likely to meet. I’ve gotten to travel and live in many different places, to seriously try my hand at many things, and to encounter and befriend so many interesting people. 
+                i have no idea what i'm doing
             </p>
             <p>
-                Code has been my vocation for decades now and there are some things I’ve done that I’m proud of, particularly open source work in cryptography and internationalization. Over my career, a theme has emerged of helping people create and collaborate at large scale. I also greatly enjoy creating maps and other visualizations to get an overview effect of what humanity is up to.
+
             </p>
             <p>
-                At other times, I’ve gotten obsessed with design, typography, music, journalism, activism, bicycling, running, and cooking.
+
             </p>
             <p>
-                About 10 years ago I met the love of my life and I’m planning to be obsessed with her forever. We live in Vancouver, Canada.
+                
             </p>
         </div> 
     @endsection
